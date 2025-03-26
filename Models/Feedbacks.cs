ed943f68c0a575751d160af9e1c9fa809526440c
nhaq0505 <anhquan050503@gmail.com>
2025-03-26 14:23:30 +0700

    update

--- Models/Feedbacks.cs
+++ Models/Feedbacks.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validate;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace TVOnline.Models {
     public class Feedbacks {
