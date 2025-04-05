02d3a89f04bb24a0d7a000c6b24257c296050528
Anh Quân <anhquan050503@gmail.com>
2025-04-06 04:14:16 +0700

    mergeCode

---
+++ Migrations/20250402225740_AddRatingToFeedback.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TVOnline.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRatingToFeedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Feedbacks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Feedbacks");
+        }
+    }
+}
